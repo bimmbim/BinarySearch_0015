8a3c1526729cd1c2eeda149db326a74437d2704e
bimmbim <bimabaraja76@gmail.com>
2025-05-07 19:12:34 +0700

    commit7

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -68,6 +68,19 @@ void display()
 void binarySearch()
 {
     char ulang;
-    
+    do
+    {
+        cout << "\n=========================================\n";
+        cout << "         Pencarian Binary Search         \n";
+        cout << "=========================================\n";
+        cout << "Masukkan elemen yang ingin dicari: ";
+        cin >> x;
+
+        int low = 0;
+        int high = nPanjang - 1;
+        
+
+        
+    }
  while (ulang == 'y' || ulang == 'Y');
 }
